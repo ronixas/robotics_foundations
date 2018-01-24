ba8e71eb0f7505cd97e50734ee88063476658b2e
Ronaldas <2143806g@student.gla.ac.uk>
2018-01-24 18:13:04 +0000

    add reading_ranges.py

---
+++ robot_ws/src/myturtlebot/src/reading_ranges.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import rospy
+
+# Data structure here: http://docs.ros.org/api/sensor_msgs/html/msg/LaserScan.html
+from sensor_msgs.msg import LaserScan
+
+# This function is called each time a new message arrives on the scan topic. This callback function then prints the range measured to the object directly in front of the robot by picking the middle element of the ranges field of the LaserScan message
+def scan_callback(msg):
+    range_ahead = msg.ranges[len(msg.ranges)/2]
+    print "range ahead: %0.1f" % range_ahead
+
+rospy.init_node('reading_ranges')
+
+# you now subscribe to the topic, this is similar to 'rostopic echo /scan' but now the messages are channelled to 'scan_callback' rather than the screen
+scan_sub = rospy.Subscriber('scan', LaserScan, scan_callback)
+
+# spin() keeps your node from exiting until the node is shut down. This is thread independent and does not affect the execution of the callbacks
+rospy.spin()
