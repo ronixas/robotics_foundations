3f1967f679e2ab3758ca9dcc1d2be85a9bdea7f5
ronixas <2143806g@student.gla.ac.uk>
2018-01-25 13:56:58 +0000

    start fresh

--- robot_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rflab/robotics_foundations/robot_ws/src
-# Build directory: /home/rflab/robotics_foundations/robot_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(myturtlebot)
--- robot_ws/build/myturtlebot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rflab/robotics_foundations/robot_ws/src/myturtlebot
-# Build directory: /home/rflab/robotics_foundations/robot_ws/build/myturtlebot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
